823a5be0e4fb099ff2305010fc0a7ecb16538c00
TokaHamdy <doudyhamdy.th@gmail.com>
2024-12-08 02:49:23 +0200

    Use Swagger

---
+++ src/main/java/com/example/library/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.library.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API Documentation")
+                        .version("1.0.0")
+                        .description("This is the API documentation for my Spring Boot application"));
+    }
+}
